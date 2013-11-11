1cea37a54c47648d26c9315b0b739aaf07db2444
EGYP7IAN <andrzejczak.ian@gmail.com>
2013-11-11 14:55:36 -0700

    Added python server and client.

---
+++ src/project/src/painter_server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python  
+import roslib
+roslib.load_manifest('learning_tf')
+import rospy
+import tf
+import painter.srv
+
+def handle(req):
+    print "Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b))
+    return AddTwoIntsResponse(req.a + req.b)
+
+def paint():
+    rospy.init_node('add_two_ints_server')
+    s = rospy.Service('add_two_ints', AddTwoInts, handle_add_two_ints)
+    print "Ready to add two ints."
+    rospy.spin()
+
+if __name__ == "__main__":
+    add_two_ints_server()
